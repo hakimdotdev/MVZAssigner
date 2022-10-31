6856eede1f7d11f9e36d21034a5462be029a2d54
OpSecIT <104262846+opsecit@users.noreply.github.com>
2022-10-31 14:08:19 +0100

    Update zuordnung.ps1

--- zuordnung test/Resources/zuordnung.ps1
+++ zuordnung test/Resources/zuordnung.ps1
@@ -1,1 +1,12 @@
-﻿
+$Destination
+Get-ChildItem $Source | ForEach-Object {
+    $Tail = ($_.BaseName -split '_')[-2]
+    $Filter = "*_{0}" -f $Tail
+    $DestDir = Get-ChildItem $Destination -Filter $Filter -Directory | Select-Object -ExpandProperty FullName -First 1
+    if ($DestDir) {
+        Copy-Item $_.FullName $DestDir -Force
+    } else {
+        "No Directory was found that matched the Filter {0} in Directory {1}" -f $Filter, $Destination | Write-Host
+    }
+}
+
